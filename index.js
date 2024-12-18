c68000393f79e706791230f85f2656160c1f0bc2
Moritz <mopacher@tsn.at>
2024-12-18 09:17:15 +0100

    test

--- index.js
+++ index.js
@@ -140,6 +140,8 @@ function validateMessage(data) {
     return true;
 }
 
+
+
 //-----------------------------------------------------------------------------------
 //XXXXXXXXXX
 //Zur Simulation:
